54b51dc3206e843ea903458c18956969061155e8
Arnaud Renaud <arnaud.renaud@gmail.com>
2023-11-30 15:28:10 +0100

    Serve files in public directory

--- file-hosting/src/index.ts
+++ file-hosting/src/index.ts
@@ -5,6 +5,7 @@ const app = express();
 app.get("/", (req, res) => {
   return res.send("Hello");
 });
+app.use(express.static("public"));
 
 app.listen(5001, () => {
   console.log("File hosting server listening on port 5001.");
